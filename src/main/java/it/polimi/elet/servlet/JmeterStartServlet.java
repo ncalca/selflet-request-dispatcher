661b22a272f9418bf40559a2fcffcef8725f4012
elleFlorio <elle.florio@gmail.com>
2014-12-05 17:05:38 +0100

    bug fixed in jmeter start command

--- src/main/java/it/polimi/elet/servlet/JmeterStartServlet.java
+++ src/main/java/it/polimi/elet/servlet/JmeterStartServlet.java
@@ -56,7 +56,7 @@ public class JmeterStartServlet extends HttpServlet {
 		String commandLocate = "cd " + JMETER_FOLDER;
 		String commandStart = "./jmeter -n -t " + TRACK_FOLDER + TRACK_NAME
 				+ " -JdispatcherIpAddress=" + dispatcherIpAddress
-				+ " selflets_results.jtl";
+				+ " -l selflets_results.jtl";
 		connection.execute(commandLocate);
 		connection.execute(commandStart);
 
