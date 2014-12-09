8d7b3db2e1549cf9702eb59609b4393c1dcb43dd
elleFlorio <elle.florio@gmail.com>
2014-12-09 17:04:59 +0100

    test

--- src/main/resources/shell_scripts/mergeSelfletsLogs.sh
+++ src/main/resources/shell_scripts/mergeSelfletsLogs.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
 cd ../../webapp/logs/
-cat actions*.log > all_actions.csv
+cat actions*.log > all_actions.log
 rm actions*.log
-sed -i '1,1s/^/timestamp,action \n/g' all_actions.csv
-mv all_actions.csv ../results/
+sed -i '1,1s/^/timestamp,action \n/g' all_actions.log
+mv all_actions.log ../results/
 
-cat results*.log > all_results.csv
+cat results*.log > all_results.log
 rm results*.log
-sed -i '1,1s/^/timestamp,reqType,service,rt,numberOfReqs \n/g' all_results.csv
-mv all_results.csv ../results/
+sed -i '1,1s/^/timestamp,reqType,service,rt,numberOfReqs \n/g' all_results.log
+mv all_results.log ../results/
 
 cat selflet*.log > all_logs.log
 rm selflet*.log
