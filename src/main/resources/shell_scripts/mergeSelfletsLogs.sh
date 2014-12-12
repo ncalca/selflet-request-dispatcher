5be895dffc1865eefe766c166935e54399308a02
elleFlorio <elle.florio@gmail.com>
2014-12-12 14:39:43 +0100

    fix

--- src/main/resources/shell_scripts/mergeSelfletsLogs.sh
+++ src/main/resources/shell_scripts/mergeSelfletsLogs.sh
@@ -9,7 +9,7 @@ mv all_actions.csv ../results/
 cat results*.log > all_results.csv
 rm results*.log
 sed -i '1,1s/^/timestamp,reqType,service,rt,numberOfReqs \n/g' all_results.csv
-mv all_results.log ../results/
+mv all_results.csv ../results/
 
 cat selflet*.log > all_logs.log
 rm selflet*.log
