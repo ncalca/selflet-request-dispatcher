d2cf1be5a8cd80f0e9e97be4e8ff2cff836fc856
elleFlorio <elle.florio@gmail.com>
2015-02-01 16:54:40 +0100

    added the logging of selflets lifetime

--- src/main/resources/shell_scripts/mergeSelfletsLogs.sh
+++ src/main/resources/shell_scripts/mergeSelfletsLogs.sh
@@ -11,6 +11,21 @@ rm results*.log
 sed -i '1,1s/^/timestamp,reqType,service,rt,selflet,numberOfReqs \n/g' all_results.csv
 mv all_results.csv ../results/
 
+cat start*.log > all_start.csv
+rm start*.log
+sed -i '1,1s/^/timestamp,selflet \n/g' all_start.csv
+mv all_start.csv ../results/
+
+cat end*.log > all_end.csv
+rm end*.log
+sed -i '1,1s/^/timestamp,selflet \n/g' all_end.csv
+mv all_end.csv ../results/
+
+cat life*.log > all_life.csv
+rm life*.log
+sed -i '1,1s/^/timestamp,selflet,start,end,lifetime \n/g' all_life.csv
+mv all_life.csv ../results/
+
 cat selflet*.log > all_logs.log
 rm selflet*.log
 mv all_logs.log ../results/
